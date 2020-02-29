150639179344020acf13a1b7b0cf9e3a9ae855cc
prokomone01 <prokomoneone1@gmail.com>
2020-03-01 01:49:48 +0700

    first commit

---
+++ App.java
@@ -0,0 +1,14 @@
+
+public class App {
+    public static void main(String args[]){
+        permukaanluasbola z;
+        
+        z = new permukaanluasbola();
+        
+        z.luaspermukaanbola();
+        
+        
+        
+    }
+    
+}
---
+++ permukaanluasbola.java
@@ -0,0 +1,28 @@
+
+public class permukaanluasbola {
+        double phi = 3.14;
+        int r1 = 5;
+        int r2 = 10;
+        int m = 4;
+        double persegi = Math.pow(r1, 2);
+        double persegi2 = Math.pow(r2, 2);
+        int LuasPermukaanbola = (int) (m * phi * persegi);
+    private double r;
+        
+        public permukaanluasbola(){
+        
+        }
+ 
+        public void luaspermukaanbola(){
+        
+        System.out.println(" hitunglah luas Permukaan bola dengan jari-jari = " + r1);
+        System.out.println(" hitunglah luas Permukaan bola dengan jari-jari = " + r2);
+        System.out.println("luas permukaan bola");
+        System.out.println ( + m + "*" + persegi + "*" + phi + " = 314");
+        System.out.println ( + m + "*" + persegi2 + "*" + phi + " = 1256");
+        
+        
+        
+}
+}
+   
